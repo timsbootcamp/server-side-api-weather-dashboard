a2728b3011525df564407544875b8afd96113bfc
Arvinder <timsbootcamp@gmail.com>
2024-01-27 10:16:29 +0000

    Get 5 day forecast working in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,64 @@ fetch(queryURL)
     .then(function (data) {
 
         latitude = data.coord.lat;
-        longitude = data.coord.lon;                                      
+        longitude = data.coord.lon;
+
+        let query2URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_Key_OpenWeatherMap}`;
+        return fetch(query2URL);
+    })
+    .then(response => response.json())
+    .then(data2 => {
+        let forecastToday = getWeatherDetails(data2.list[0]);
+        let forecastFiveDaysArray = filterForecastNextFiveDays(data2);
     });
 
-    
+
+
+    function getWeatherDetails(data) {
+
+    return {
+        weatherDate: getDateInFormat_ddmmyyyy(data.dt_txt),
+        temperatureCelsius: data.main.temp,
+        windSpeedKPH: data.wind.speed * 3.6,
+        humidityPercent: data.main.humidity
+    };
+}
+
+
+
+function filterForecastNextFiveDays(data) {
+
+    // Identify last date and time available in last entry
+    let lastDate = data.list[data.list.length - 1].dt_txt;
+    timeAvailableInLastDate = getTimeFromDateString(lastDate);
+
+
+    // Now filter data2 to get just forecast data for that time each day
+    let filter = data.list.filter(item => {
+        const date = new Date(item.dt * 1000);
+        return date.getHours() === timeAvailableInLastDate;
+    })
+
+    let forecastArray = [];
+
+    filter.forEach(function (element) {
+        let forecast = getWeatherDetails(element);
+        forecastArray.push(forecast);
+    });
+
+    return forecastArray;
+
+}
+
+
+function getDateInFormat_ddmmyyyy(dt) {
+    const dayjsInstance = dayjs(dt);
+    return dayjsInstance.format("DD/MM/YYYY");
+}
+
+
+function getTimeFromDateString(dateIncludingTime) {
+    const dayjsInstance = dayjs(dateIncludingTime);
+    return dayjsInstance.hour();
+}   
+
